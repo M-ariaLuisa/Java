8fc813da2b70c03f529115122026ac59424109c3
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-16 15:43:02 -0300

    salvar

--- 1-Atividade/script.js
+++ 1-Atividade/script.js
@@ -1,9 +1,27 @@
 function gerarTabuada() {
     // Obtendo o número do Input no HTML.
     const numeroInput = document.getElementById("numeroInput")
-    let numero = parent(numeroInput.value)
+    let numero = parseInt(numeroInput.value);
 
     // Elemento do HTML onde o resultado será exibido.
-    const resultadoDiv = document.getElementById("resultadoTabuada")
+    const resultadoDiv = document.getElementById("resultadoTabuada");
     resultadoDiv.innerHTML = "";
+
+    if(isNaN(numero) || numero === null || numero === ""){ (
+        resultadoDiv.innerHTML = "<p> Porfavor, digite um número válido <p>";
+        return; // Este comando sai da função.
+
+    }
+    // Adicionar título para a tabuada
+    resultadoDiv.innerHTML += `<h2>tabuada do número $(numero)</h2>`;
+
+    // Laço de repetição para gerar tabuada
+    for (let i = 1; i <= 10, i++) {
+        let resultado = numero = i;
+        resultadoDiv.innerHTML += `<p> $(numero) x $(i) - $(resultado) </p>`;
+
+    }
 }
+const botaoGerar = document.getElementById("btnGerar");
+botaoGerar.addEventListener("click",gerarTabuada);
+
